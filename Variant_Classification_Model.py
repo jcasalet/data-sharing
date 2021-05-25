82850c1f67b9656ba51a4791080327212462c827
James Casaletto <jcasaletto@maprtech.com>
2021-05-25 09:30:27 -0700

    changing union

--- Variant_Classification_Model.py
+++ Variant_Classification_Model.py
@@ -131,6 +131,56 @@ class Configuration:
             jsonData = myFile.read()
         self.data = json.loads(jsonData)
 
+def calculateProbabilityOfUnion(year, subsetByLenDict):
+    # subsetByLenDict = {'1': [(a,), (b,), (c,)], '2': [(a, b), (a, c), (b, c)], '3': [(a, b, c)]}
+    probabilities = dict()
+    for length in subsetByLenDict:
+        probabilities[length] = {'LB': 0, 'B':0, 'LP': 0, 'P':0}
+        for subset in subsetByLenDict[length]:
+            tempLB = 1.0
+            tempB = 1.0
+            tempLP = 1.0
+            tempP = 1.0
+            for center in subset:
+                tempLB *= center.likelyBenignProbabilities[year]
+                tempB *= center.benignProbabilities[year]
+                tempLP *= center.likelyPathogenicProbabilities[year]
+                tempP *= center.pathogenicProbabilities[year]
+            probabilities[length]['LB'] += tempLB
+            probabilities[length]['B'] += tempB
+            probabilities[length]['LP'] += tempLP
+            probabilities[length]['P'] += tempP
+
+    probabilityUnion = {'LB': 0, 'B': 0, 'LP':0, 'P':0}
+    for length in probabilities:
+        if int(length) % 2 == 1:
+            probabilityUnion['LB'] += probabilities[length]['LB']
+            probabilityUnion['B'] += probabilities[length]['B']
+            probabilityUnion['LP'] += probabilities[length]['LP']
+            probabilityUnion['P'] += probabilities[length]['P']
+        else:
+            probabilityUnion['LB'] -= probabilities[length]['LB']
+            probabilityUnion['B'] -= probabilities[length]['B']
+            probabilityUnion['LP'] -= probabilities[length]['LP']
+            probabilityUnion['P'] -= probabilities[length]['P']
+
+    return probabilityUnion
+
+
+    # {'1':    {'LB': P(LB(a)) + P(LB(b) + P(LB(c)),
+    #           'B': P(B(a)) + P(B(b)) + P(B(c)),
+    #           'LP': P(LP(a)) + P(LP(b)) + P(LP(c))
+    #           'P': P(P(a)) + P(P(b)) + P(P(c))}
+    # '2':      {'LB': P(LB(a))*P(LB(b)) + P(LB(a))*P(LB(c)) + P(LB(b))*P(LB(c)),
+    #           'B': P(B(a))*P(B(b)) + P(B(a))*P(B(c)) + P(B(b))*P(B(c)),
+    #           'LP': P(LP(a))*P(LP(b)) + P(LP(a))*P(LP(c)) + P(LP(b))*P(LP(c)),
+    #           'P': P(P(a))*P(P(b)) + P(P(a))*P(P(c)) + P(P(b))*P(P(c))},
+    #  ...
+    # '20': },
+
+    # {'size': {LB: sum-prod-probs, B: sum-prod-probs, LP: sum-prod-probs, P: sum-prod-probs}
+
+
 
 class Simulation:
     def __init__(self, config, saType, saParam):
@@ -309,6 +359,34 @@ class Simulation:
         # probability of classification of any of the centers (i.e. P(A or B or C or ...))
         # use https://en.wikipedia.org/wiki/Inclusion%E2%80%93exclusion_principle#In_probability
         self.inclusionExclusionProbability()
+        self.unionProbability()
+
+    def unionProbability(self):
+        # P(A U B U C ... U Z) = (sum size 1 sets) - (sum size 2 sets) + (sum size 3 sets) - ...
+        import itertools
+        subsets = list()
+        centers = set()
+        for cl in self.centerListList:
+            for c in cl:
+                centers.add(c)
+        for i in range(1, len(centers) + 1):
+            subsets.append(itertools.combinations(centers, i))
+
+        # {'1': [(a,), (b,), (c,)], '2': [(a, b), (a, c), (b, c)], '3': [(a, b, c)]}
+        subsetByLenDict = dict()
+        for subset in subsets:
+            for s in subset:
+                l = str(len(s))
+                if not str(l) in subsetByLenDict:
+                    subsetByLenDict[l] = []
+                subsetByLenDict[l].append(s)
+
+        # subsetByLenDict = {'1': [(a,), (b,), (c,)], '2': [(a, b), (a, c), (b, c)], '3': [(a, b, c)]}
+        probabilityUnions = []
+        for year in self.years:
+            probabilityUnions.append(calculateProbabilityOfUnion(year, subsetByLenDict))
+
+        print(probabilityUnions)
 
     def inclusionExclusionProbability(self):
         # https://en.wikipedia.org/wiki/Inclusion%E2%80%93exclusion_principle#In_probability
@@ -347,6 +425,11 @@ class Simulation:
                 Bprob *= (1.0 - B[center][year])
                 LPprob *= (1.0 - LP[center][year])
                 Pprob *= (1.0 - P[center][year])
+            # here is the problem.
+            # recall that P(LB) + P(B) <= 1 for any center.
+            # so it's false that LB's add to 1 for a center
+            # so subtracting from 1 is wrong
+
             self.LBanyCenter.append(1 - LBprob)
             self.BanyCenter.append(1 - Bprob)
             self.LPanyCenter.append(1 - LPprob)
@@ -833,6 +916,7 @@ def main():
         mySimulation.hist(outputDir=outputDir)
         mySimulation.prob(outputDir=outputDir)
         plotAnyCenterProbability(mySimulation, outputDir)
+
 
     elif jobType == 'analyze':
         print('analyze this!')
